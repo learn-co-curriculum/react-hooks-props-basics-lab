974fb51f7219876f9bbede6e9a7182f834072eec
Sylwia Vargas <45401242+sylwiavargas@users.noreply.github.com>
2020-01-28 09:19:51 -0500

    Update ColorBox.js

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -4,7 +4,7 @@ export default class ColorBox extends Component {
 
   render() {
     return (
-      <div className="color-box" style={{opacity: 1}}>
+      <div className="color-box" style={{opacity: null /*replace null with the value*/}}>
         {/* your conditional code here! */}
       </div>
     )
