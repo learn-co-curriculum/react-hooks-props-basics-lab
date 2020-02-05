255fb7805d16c31c35f2a60a82c2d8ce57541f3b
sylwiavargas <sylwia.vargas@gmail.com>
2020-02-05 11:31:37 -0500

    update blogpost

--- src/BlogPost.js
+++ src/BlogPost.js
@@ -5,20 +5,19 @@ export default class BlogPost extends Component {
   render() {
     
     const comments = [
-      "Tell my wifi love her", 
-      "Ken M was here",
-      "Did you ever hear the tragedy of Darth Plagueis The Wise? I thought not. It's not a story the Jedi would tell you. It's a Sith legend. Darth Plagueis was a Dark Lord of the Sith, so powerful and so wise he could use the Force to influence the midichlorians to create life… He had such a knowledge of the dark side that he could even keep the ones he cared about from dying. The dark side of the Force is a pathway to many abilities some consider to be unnatural. He became so powerful… the only thing he was afraid of was losing his power, which eventually, of course, he did. Unfortunately, he taught his apprentice everything he knew, then his apprentice killed him in his sleep. Ironic. He could save others from death, but not himself."
+      "When we speak we are afraid our words will not be heard or welcomed. But when we are silent, we are still afraid. So it is better to speak. - Audrey Lorde", 
+      "I am no longer accepting the things I cannot change. I am changing the things I cannot accept. - Angela Davis",
+      "If you don't understand, ask questions. If you're uncomfortable about asking questions, say you are uncomfortable about asking questions and then ask anyway. It's easy to tell when a question is coming from a good place. Then listen some more. Sometimes people just want to feel heard. Here's to possibilities of friendship and connection and understanding. - Chimamanda Ngozi Adichie"
     ]
     
     return (
       <div id="blog-post" className="wrapper">
-        Alright internet trolls<br/>
-        Come at me<br/>
-        I have been preparing<br/>
-        My meme fortress is built<br/>
-        I won't let you tilt me<br/>
-        You only grew accustomed to the salt<br/>
-        I was born in it<br/>
+        Just like moons and like suns,<br/>
+        With the certainty of tides,<br/>
+        Just like hopes springing high,<br/>
+        Still I'll rise.<br/>
+        -Maya Angelou<br/>
+
         {/* (remove the comment ticks and their surrounding brackets)
         <Comment commentText={comments[0]} />
         <Comment commentText={comments[0]} />
--- src/ColorBox.js
+++ src/ColorBox.js
@@ -2,7 +2,14 @@ import React, { Component } from 'react';
 
 export default class ColorBox extends Component {
 
+  state = {
+    todos: [
+      
+    ]
+  }
+
   render() {
+    console.log(this)
     return (
       <div className="color-box" style={{opacity: null /*replace null with the value*/}}>
         {/* your conditional code here! */}
