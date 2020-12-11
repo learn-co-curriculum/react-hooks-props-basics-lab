8f6f554743679bd29a3f22530040d5b4df907869
ihollander <ianholla@gmail.com>
2020-12-11 11:13:08 -0500

    Fixed typos

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,7 @@ import About from "./About";
 const user = {
   name: "Liza",
   city: "New York",
-  bio: "I made this",
+  bio: "I made this!",
   color: "firebrick",
   links: {
     github: "https://github.com/liza",
