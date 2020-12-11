6ccc43c7606c4567ee994447cc6a51355b6e50aa
ihollander <ianholla@gmail.com>
2020-12-11 11:10:05 -0500

    Added solution

--- src/components/About.js
+++ src/components/About.js
@@ -1,13 +1,13 @@
 import React from "react";
+import Links from "./Links";
 
-function About() {
+function About(props) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>I made this</p>
+      {props.bio && props.bio.length > 1 ? <p>{props.bio}</p> : null}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* Uncomment the line below once you've made your <Links /> component */}
-      {/* <Links /> */}
+      <Links github={props.links.github} linkedin={props.links.linkedin} />
     </div>
   );
 }
--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,7 @@ import About from "./About";
 const user = {
   name: "Liza",
   city: "New York",
-  bio: "I made this",
+  bio: "I made this!",
   color: "firebrick",
   links: {
     github: "https://github.com/liza",
@@ -19,8 +19,8 @@ function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home username={user.name} city={user.city} color={user.color} />
+      <About bio={user.bio} links={user.links} />
     </div>
   );
 }
