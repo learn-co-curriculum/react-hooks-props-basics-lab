33952e2076df47fd12edc8441b2cc5c8fff19bcc
ihollander <ianholla@gmail.com>
2020-12-11 11:01:24 -0500

    Updated domain and deliverables

---
+++ src/__test__/Home.test.js
@@ -0,0 +1,23 @@
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+
+import Home from "../components/Home";
+
+test("uses the 'username' and 'city' props to display the correct text", () => {
+  render(<Home username="Liza" city="New York" color="firebrick" />);
+  const liza = screen.queryByText(`Liza is a Web Developer from New York`);
+  expect(liza).toBeInTheDocument();
+
+  render(<Home username="Duane" city="Queens" color="firebrick" />);
+  const duane = screen.queryByText(`Duane is a Web Developer from Queens`);
+  expect(duane).toBeInTheDocument();
+});
+
+test("uses the 'color' prop to set the inline style color of the h1", () => {
+  const { container } = render(
+    <Home username="Liza" city="New York" color="firebrick" />
+  );
+  const h1 = container.querySelector("h1");
+  expect(h1).toBeInTheDocument();
+  expect(h1.style.color).toEqual("firebrick");
+});
