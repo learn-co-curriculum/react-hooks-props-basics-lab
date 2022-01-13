36fb6d239ec39b2ff127fa7b33e79f7cce1aa14c
Ian Hollander <39830572+ihollander@users.noreply.github.com>
2022-01-13 11:34:33 -0500

    Clarify instructions (#6)
    Update tests and solutions based on clarified instructions

--- src/components/App.js
+++ src/components/App.js
@@ -5,13 +5,12 @@ import About from "./About";
 
 // pass this data down as props to the child component(s) that need it!
 import user from "../data/user";
-console.log(user);
 
 function App() {
   return (
     <div>
       <NavBar />
-      <Home username={user.name} city={user.city} color={user.color} />
+      <Home name={user.name} city={user.city} color={user.color} />
       <About bio={user.bio} links={user.links} />
     </div>
   );
