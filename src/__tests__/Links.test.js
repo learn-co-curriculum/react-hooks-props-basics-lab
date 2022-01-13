d361419e2b11b0c1a4a66fa2b22631af0181ee2c
Ian Hollander <ianholla@gmail.com>
2022-01-13 11:32:25 -0500

    Update <Links> test descriptions

--- src/__tests__/Links.test.js
+++ src/__tests__/Links.test.js
@@ -8,12 +8,12 @@ test("renders the h3 with the text 'Links'", () => {
   expect(screen.queryByText("Links")).toBeInTheDocument();
 });
 
-test("displays a Github link passed down as a prop", () => {
+test("displays the URL of a Github link passed down as a prop", () => {
   render(<Links github={"https://github.com/liza"} />);
   expect(screen.queryByText("https://github.com/liza")).toBeInTheDocument();
 });
 
-test("displays a Linkedin link passed down as a prop", () => {
+test("displays the URL of a Linkedin link passed down as a prop", () => {
   render(<Links linkedin={"https://www.linkedin.com/in/liza/"} />);
   expect(
     screen.queryByText("https://www.linkedin.com/in/liza/")
