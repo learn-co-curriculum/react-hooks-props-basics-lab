3531bacef54ae09c0c1e6e3efcaa4b7e9eec25c8
Keila Lopez <keilalofra@gmail.com>
2022-10-30 12:10:16 -0600

    first and last commit

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,17 @@
 import React from "react";
+import Links from "./Links"
 
-function About() {
+function About({bio, links}) {
+
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {bio && bio.length > 0 ? <p>{bio}</p> : null }
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links github={links.github} linkedin={links.linkedin}/>
     </div>
-  );
+  )
 }
 
 export default About;
+
--- src/components/App.js
+++ src/components/App.js
@@ -11,8 +11,8 @@ function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home name={user.name} city={user.city} color={user.color}/>
+      <About bio={user.bio} links={user.links} />
     </div>
   );
 }
